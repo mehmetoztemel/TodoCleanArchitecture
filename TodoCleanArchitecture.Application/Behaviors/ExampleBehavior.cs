ef5880541ed5b4fb400b785c5c030f636846ed4e
Mehmet Öztemel <mehmet-oztemel@hotmail.com>
2024-07-29 21:29:02 +0300

    Fluent Validation

---
+++ TodoCleanArchitecture.Application/Behaviors/ExampleBehavior.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TodoCleanArchitecture.Application.Behaviors
+{
+    public class ExampleBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : class, IRequest<TResponse>
+    {
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TodoCleanArchitecture.Application/DependencyInjection.cs
+++ TodoCleanArchitecture.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using TodoCleanArchitecture.Application.Behaviors;
 
 namespace TodoCleanArchitecture.Application
 {
@@ -11,6 +12,9 @@ namespace TodoCleanArchitecture.Application
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+                // bütün yapıda kullanacaksak bunu ekliyoruz.
+                //configuration.AddOpenBehavior(typeof(ExampleBehavior<,>));
+                configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
             });
 
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
